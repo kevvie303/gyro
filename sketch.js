a7e476ada8ca9b266a91fa60e18e951cd5b05e1e
Kevin van Baar <kvbaar@hotmail.nl>
2023-02-10 10:54:16 +0100

    fixing the borders

--- sketch.js
+++ sketch.js
@@ -9,6 +9,7 @@ function setup() {
 function draw() {
   background(255);
   ellipse(x, y, 50, 50);
+  borders();
 }
 
 window.addEventListener("deviceorientation", handleOrientation);
@@ -16,7 +17,20 @@ window.addEventListener("deviceorientation", handleOrientation);
 function handleOrientation(event) {
   let tiltLR = event.gamma;
   let tiltFB = event.beta;
-
   x += 0.2 * tiltLR;
   y += 0.2 * tiltFB;
+}
+function borders() {
+  if (x >= window.innerWidth - 50) {
+    x = window.innerWidth - 50;
+  }
+  if (x <= 0 &! y >= window.innerHeight + 150 &! y <= window.innerHeight) {
+    x = 0;
+  }
+  if (y <= 0) {
+    y = 0;
+  }
+  if (y >= window.innerHeight + 50) {
+    y = window.innerHeight + 50
+  }
 }
