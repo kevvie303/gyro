2840a7fa79f50593f23892ee7553d5b2cd326067
Kevin van Baar <kvbaar@hotmail.nl>
2023-02-10 11:40:40 +0100

    Added random rects and lines

--- sketch.js
+++ sketch.js
@@ -1,17 +1,39 @@
-let x = window.innerWidth / 2;
-let y = window.innerHeight / 2;
-let mazeImg;
-function preload() {
-  mazeImg = loadImage("images/maze.png")
-}
+let x = 50;
+let y = 50;
+let diameter = 50;
+
 function setup() {
   createCanvas(window.innerWidth, window.innerHeight);
-  fill(255, 0, 0);
 }
 
 function draw() {
-  background(mazeImg);
-  ellipse(x, y, 50, 50);
+  background(220);
+
+  // Draw the walls of the maze
+  stroke(0);
+  strokeWeight(5);
+  rect(100, 100, 300, 300);
+  rect(200, 400, 100, 100);
+  line(600, 100, 400, 400);
+
+  // Draw the circle
+  push();
+  fill(255, 0, 0);
+  ellipse(x, y, diameter, diameter);
+  pop();
+
+  // Check for collision with the walls of the maze
+  if (collideRectCircle(100, 100, 300, 300, x, y, diameter)) {
+    x -= 5;
+  }
+
+  if (collideRectCircle(200, 400, 100, 100, x, y, diameter)) {
+    x += 5;
+  }
+
+  if (collideLineCircle(400, 100, 400, 400, x, y, diameter)) {
+    y -= 5;
+  }
 }
 
 window.addEventListener("deviceorientation", handleOrientation);
@@ -19,8 +41,9 @@ window.addEventListener("deviceorientation", handleOrientation);
 function handleOrientation(event) {
   let tiltLR = event.gamma;
   let tiltFB = event.beta;
-  x += 0.2 * tiltLR;
-  y += 0.2 * tiltFB;
+
+  x += tiltLR;
+  y -= tiltFB;
 
   x = constrain(x, 25, width - 25);
   y = constrain(y, 25, height - 25);
